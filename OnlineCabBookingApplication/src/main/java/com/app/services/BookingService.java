89ae14c95dcdc7c996be97c4298efd740f26c3dc
vaishkiyarminal001 <vaishkiyarminal001@gmail.com>
2023-11-08 20:40:44 +0530

    Added Customer Cab Driver Booking Service Implemention

--- OnlineCabBookingApplication/src/main/java/com/app/services/BookingService.java
+++ OnlineCabBookingApplication/src/main/java/com/app/services/BookingService.java
@@ -6,12 +6,12 @@ import com.app.exceptions.BookingException;
 import com.app.models.Booking;
 
 public interface BookingService {
-    Booking insertBooking(Long customerId, Long driverId, Booking booking) throws BookingException;
-    Booking updateBooking(Long customerId, Long bookingId,  Booking booking) throws BookingException ;
-    Booking deleteBooking(Long customerId, Long bookingId) throws BookingException;
-    List<Booking> getAllBookingsDriver(Long driverId) throws BookingException;
-    List<Booking> getBookingsByCustomer(Long customerId) throws BookingException;
-    List<Booking> getBookingsByCab(Long cabId) throws BookingException;
-   
+	  Booking insertBooking(Long customerId, Long driverId, Booking booking) throws BookingException;
+	    Booking updateBooking(Long customerId, Long bookingId,  Booking booking) throws BookingException ;
+	    Booking deleteBooking(Long customerId, Long bookingId) throws BookingException;
+	    List<Booking> getAllBookingsDriver(Long driverId) throws BookingException;
+	    List<Booking> getBookingsByCustomer(Long customerId) throws BookingException;
+	    List<Booking> getBookingsByCab(Long cabId) throws BookingException;
+	   
 
 }
