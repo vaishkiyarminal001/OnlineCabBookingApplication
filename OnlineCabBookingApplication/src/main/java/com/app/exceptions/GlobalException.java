23aa80885c09464862e0b0c5a55f8c1eef7724ef
vaishkiyarminal001 <vaishkiyarminal001@gmail.com>
2023-11-08 20:03:50 +0530

    Added exception

--- OnlineCabBookingApplication/src/main/java/com/app/exceptions/GlobalException.java
+++ OnlineCabBookingApplication/src/main/java/com/app/exceptions/GlobalException.java
@@ -1,9 +1,12 @@
 package com.app.exceptions;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -47,6 +50,21 @@ public class GlobalException {
 			myErrorDetails.setDiscription(wr.getDescription(false));
 			return new ResponseEntity<MyErrorDetails>(myErrorDetails, HttpStatus.BAD_REQUEST);
 		}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<MyErrorDetails> handleValidationeException(MethodArgumentNotValidException ex, WebRequest wr){
+		MyErrorDetails myErrorDetails = new MyErrorDetails();
+		myErrorDetails.setTimeStamp(LocalDate.now());
+		
+		myErrorDetails.setMessage("Validation failed !!");
+		List<ObjectError> allErrors = ex.getBindingResult().getAllErrors();
+
+		List<String> errorMessages = MethodArgumentNotValidException.errorsToStringList(allErrors);
+		
+
+		myErrorDetails.setDiscription(String.join(", ", errorMessages));
+		return new ResponseEntity<MyErrorDetails>(myErrorDetails, HttpStatus.BAD_REQUEST);
+	}
 	}
 	
 
